9d0b158d5e135a35291fba7ee10d9ef53f6581bc
Lev Mitchell <140019717+Inocybe@users.noreply.github.com>
2026-08-07 17:13:39 -0700

    Initial Commit

---
+++ learning-metal/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learning-metal
+//
+//  Created by Lev Mitchell on 8/7/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
